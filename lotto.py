6bee00c9840a1c81fed85d2418723da506380c4f
Filip Michalski <filip.m8chalski@gmail.com>
2026-02-26 14:12:45 +0100

    Dodano liczenie i wyswietlenie ilosci wystapien wylosowanych liczb wg systemow losowania jako podsumowanie dzialania programu.

--- lotto.py
+++ lotto.py
@@ -34,17 +34,35 @@ def losowanko(przedzialy, system=[2,1,1,1,1]): #losowanie
     jaki_system(system)
     print(liczby)
     print("")
+    return liczby
 
+def podsumowanie(ilosc, liczby): #liczenie z uzyciem slownika
+    for i in liczby:
+        ilosc[i] = ilosc.get(i, 0) + 1
+    return ilosc
+
+def wyswietl_podsumowanie(ilosc):
+    posortowany = dict(sorted(ilosc.items(), key=lambda item: item[1], reverse=True))
+    print(f"\n========Wystąpienia liczb========")
+    k = "raz(y)."
+    for i in posortowany:
+        print(f"Liczba {i:>2} wystąpiła: {str(posortowany[i]):>1} {k:>8}")
+    print(f"==================================")
 przedzialy = [range(1, 10),
         range(10, 20),
         range(20, 30),
         range(30, 40),
         range(40, 50)]
 
-losowanko(przedzialy) #jedna para 2-1-1-1-1
-losowanko(przedzialy, [2, 2, 2]) #2-2-2
-losowanko(przedzialy, [3, 2, 1]) #3-2-1
-losowanko(przedzialy, [3, 3]) #3-3
-losowanko(przedzialy, [2, 2, 1, 1])#2-2-1-1
-losowanko(przedzialy, [3, 1, 1, 1])#3-1-1-1
-losowanko(przedzialy, [4, 1, 1])#4-1-1
+ilosc = dict()
+
+podsumowanie(ilosc,losowanko(przedzialy)) #jedna para 2-1-1-1-1
+podsumowanie(ilosc,losowanko(przedzialy, [2, 2, 2])) #2-2-2
+podsumowanie(ilosc,losowanko(przedzialy, [3, 2, 1])) #3-2-1
+podsumowanie(ilosc,losowanko(przedzialy, [3, 3])) #3-3
+podsumowanie(ilosc,losowanko(przedzialy, [2, 2, 1, 1])) #2-2-1-1
+podsumowanie(ilosc,losowanko(przedzialy, [3, 1, 1, 1])) #3-1-1-1
+podsumowanie(ilosc,losowanko(przedzialy, [4, 1, 1])) #4-1-1
+wyswietl_podsumowanie(ilosc)
+#losowanko(przedzialy, [5, 1]) #5-1
+#losowanko(przedzialy, [6]) #6 identycznych
